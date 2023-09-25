6ec7e2776c87b3faff47f05e3cc70d4facbf2a2d
Derek Rodriguez <derek.rodriguez@pm.me>
2023-09-24 17:07:23 -0700

    got results page generation working

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+from functools import lru_cache
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, List
+from string import Template
 
 import rapidfuzz
 from fastapi import FastAPI, File, UploadFile
@@ -22,23 +24,36 @@ CARD_NAMES = list(CARD_DB.keys())
 BASIC_LAND_NAMES = ["Plains", "Swamp", "Forest", "Mountains", "Island"]
 
 
+def generate_land_to_hand_table_html():
+    pass
+
+
+@lru_cache
+def load_results_template():
+    with open("templates/results_page.html") as fp:
+        return Template(fp.read())
+
+
 @app.post("/search", response_class=HTMLResponse)
 async def process_decklist(user_decklist: UploadFile = File(...)):
-    land_names = []
+    land_names: List[str] = []
+    deck = {}
+    land_total = 0
     for encoded_line in user_decklist.file.readlines():
         line = encoded_line.decode()
-        basic_land_total = 0
         if any(char.isalnum() for char in line):
             first_space = line.find(" ")
             qty = int(line[:first_space])
             name = line[first_space + 1 :].strip()
             name_result, *_ = rapidfuzz.process.extractOne(name, choices=CARD_NAMES)
+            deck[name_result] = qty
             if CARD_DB[name_result]["layout"] == "normal":
-                if "{T}: Add {" in CARD_DB[name_result]["oracle_text"]:
-                    qty += basic_land_total
+                if "Land" in CARD_DB[name_result]["type_line"]:
+                    land_total += qty
                     land_names.append(name_result)
-    print(land_names)
-    return "\n".join(land_names)
+    return load_results_template().safe_substitute(
+        LAND_TO_HAND_TABLE="nothing", MAIN_SPEED_TABLE="nothing"
+    )
 
 
 @app.get("/", response_class=HTMLResponse)
